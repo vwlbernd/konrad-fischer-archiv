52906fc4b656c763c6f5c57238d50c5d4b517036
Daniel <daniel@xmg.fusion.fedora>
2026-02-13 18:19:34 +0100

    gitignore hinzugefügt und Dateien bereinigt

--- hooks.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-from mkdocs.config import Config
-
-def on_post_build(config: Config):
-    # Pfad zum Quellordner (außen)
-    site_medien_dir = os.path.join(config['site_dir'], 'medien')
-    source_medien_dir = os.path.join(os.path.dirname(config.config_file_path), 'medien')
